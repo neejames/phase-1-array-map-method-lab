20beae367bba28e6de95eb3e4cfe23aa1db9297c
Neechelle James <neechelle.james@gmail.com>
2023-05-06 21:05:43 -0400

    complete lab

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+
+
+
+const titleCased = function() {
+  return (tutorials.map(str => str.split(' ').map(function (word) {
+    return (word.charAt(0).toUpperCase() + word.slice(1));
+  }).join(' ')))
 }
